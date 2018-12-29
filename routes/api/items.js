71c6963067c9ca5b4fcc049681cbbb1fbd9018ec
Laksh <lakshyajeet22@gmail.com>
2018-12-29 19:04:12 +0530

    Buf Fix

--- routes/api/items.js
+++ routes/api/items.js
@@ -1,21 +1,21 @@
 const express = require('express');
 const router = express.Router();
 
-//Bring in the item model
+// Item Model
 const Item = require('../../models/Item');
 
-//@route GET api/items
-//@desc Get all items
-//@access Public
+// @route   GET api/items
+// @desc    Get All Items
+// @access  Public
 router.get('/', (req, res) => {
   Item.find()
-  .sort({ date: -1})
-  .then(items => res.json(items));
+    .sort({ date: -1 })
+    .then(items => res.json(items));
 });
 
-//@route POST api/items
-//@desc Create an item
-//@access Public
+// @route   POST api/items
+// @desc    Create An Item
+// @access  Public
 router.post('/', (req, res) => {
   const newItem = new Item({
     name: req.body.name
@@ -24,14 +24,13 @@ router.post('/', (req, res) => {
   newItem.save().then(item => res.json(item));
 });
 
-//@route DELETE api/items
-//@desc Delete an item
-//@access Public
+// @route   DELETE api/items/:id
+// @desc    Delete A Item
+// @access  Public
 router.delete('/:id', (req, res) => {
   Item.findById(req.params.id)
-  .then(item => item.remove().then(() => res.json({success: true})))
-  .catch(err => res.status(404).json({success : false}));
+    .then(item => item.remove().then(() => res.json({ success: true })))
+    .catch(err => res.status(404).json({ success: false }));
 });
 
-//So external files can access this
 module.exports = router;
